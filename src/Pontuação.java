7ea8d0e4fe996e45d4d7d476e67061047e35d525
marcotuliosr <marcotuliopessoal@gmail.com>
2020-05-15 19:58:04 -0300

    Leitura do volume

--- src/Pontuação.java
+++ src/Pontuação.java
@@ -8,10 +8,19 @@ public class Pontuação {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
+		
+		int numeroCompras;
+		double ticket;
 
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		System.out.println("");
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		numeroCompras = sc.nextInt();
+		System.out.print("Qual o ticket médio? ");
+		ticket = sc.nextDouble();
 	}
 
 }
