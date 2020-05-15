1748c5a7e141b951bd53c55f9c97e7f2fb7cff48
marcotuliosr <marcotuliopessoal@gmail.com>
2020-05-15 19:37:04 -0300

    Cabecalho

--- src/Pontuação.java
+++ src/Pontuação.java
@@ -1,9 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Pontuação {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 	}
 
 }
