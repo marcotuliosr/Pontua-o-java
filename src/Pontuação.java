90b93af9ca51c4c7df2197ded71e177b7075ebfc
marcotuliosr <marcotuliopessoal@gmail.com>
2020-05-15 20:40:26 -0300

    Score de volume de compras

--- src/Pontuação.java
+++ src/Pontuação.java
@@ -11,8 +11,11 @@ public class Pontuação {
 		
 		int numeroCompras;  
 		int inadimplencia;
+		int scCompra = 0;
 		double ticket;
+		double volumeCompras;
 		char tipoPagamento;
+		
 
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
@@ -29,6 +32,22 @@ public class Pontuação {
 		inadimplencia = sc.nextInt();
 		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
 		tipoPagamento = sc.next().charAt(0);
+		
+		volumeCompras = numeroCompras * ticket;
+
+		if (numeroCompras <= 0) {
+			scCompra = 0;
+		} else if (volumeCompras <= 3000 && numeroCompras < 2) {
+			scCompra = 20;
+		} else if (volumeCompras <= 3000 && numeroCompras >= 2) {
+			scCompra = 40;
+		} else if (volumeCompras > 3000) {
+			scCompra = 60;
+		}
+		
+
+		System.out.println("");
+		System.out.printf("Score de volume de compras = %d pontos%n", scCompra);
 		
 	}
 
