16ec384a647ee988738520bb582e0684cd859d9e
marcotuliosr <marcotuliopessoal@gmail.com>
2020-05-15 20:12:40 -0300

    Leitura dos outros dados

--- src/Pontuação.java
+++ src/Pontuação.java
@@ -9,8 +9,10 @@ public class Pontuação {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
-		int numeroCompras;
+		int numeroCompras;  
+		int inadimplencia;
 		double ticket;
+		char tipoPagamento;
 
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
@@ -21,6 +23,13 @@ public class Pontuação {
 		numeroCompras = sc.nextInt();
 		System.out.print("Qual o ticket médio? ");
 		ticket = sc.nextDouble();
+		
+		System.out.println("");
+		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
+		inadimplencia = sc.nextInt();
+		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		tipoPagamento = sc.next().charAt(0);
+		
 	}
 
 }
